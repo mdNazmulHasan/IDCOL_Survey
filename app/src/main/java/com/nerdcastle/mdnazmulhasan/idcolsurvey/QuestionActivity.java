f6727f37742cbd745a7bc6260bb731ccb32eba90
mdNazmulHasan <najmul.hasan10@gmail.com>
2015-08-23 18:29:25 +0600

    multiple value pass done !!!!1

--- app/src/main/java/com/nerdcastle/mdnazmulhasan/idcolsurvey/QuestionActivity.java
+++ app/src/main/java/com/nerdcastle/mdnazmulhasan/idcolsurvey/QuestionActivity.java
@@ -16,12 +16,15 @@ import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class QuestionActivity extends AppCompatActivity{
 
@@ -34,6 +37,15 @@ public class QuestionActivity extends AppCompatActivity{
     LinearLayout mLinearLayout;
     String questionFromJson;
     String serialNmbr;
+    String isMultiple;
+    String numberFromJson;
+    TextView question;
+    JSONArray answerArray;
+    Boolean IsMultipleAnswer;
+    ArrayList<JSONObject> answerCollection=new ArrayList<>();
+    JSONArray getAnswerArray=new JSONArray();
+    JSONObject answerobject;
+
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,7 @@ public class QuestionActivity extends AppCompatActivity{
     }
 
     private void createCheckBox(int number,JSONArray answerlist) throws JSONException {
+        mLinearLayout.removeAllViews();
 
         for(int i = 0; i < number; i++) {
             CheckBox ch = new CheckBox(getApplicationContext());
@@ -93,23 +106,25 @@ public class QuestionActivity extends AppCompatActivity{
                             Toast.makeText(getApplicationContext(), String.valueOf(sizeOfQuestionBank), Toast.LENGTH_LONG).show();
                             questionFromJson = jsonObject.getString("Description");
                             serialNmbr=jsonObject.getString("SerialNo");
-                            TextView question = (TextView) findViewById(R.id.question);
-                            question.setText(serialNmbr+". "+questionFromJson);
-                            String numberFromJson = jsonObject.getString("NoOfAnswer");
+                            question = (TextView) findViewById(R.id.question);
+                            question.setText(serialNmbr + ". " + questionFromJson);
+                            numberFromJson = jsonObject.getString("NoOfAnswer");
                             number = Integer.parseInt(numberFromJson);
                             Toast.makeText(getApplicationContext(), String.valueOf(number), Toast.LENGTH_LONG).show();
 
-                            JSONArray answerArray = jsonObject.getJSONArray("AnswerList");
-                            //createCheckBox(number, answerArray);
-
-                            createRadioButton(number, answerArray);
-
-
+                            answerArray = jsonObject.getJSONArray("AnswerList");
+                            isMultiple=jsonObject.getString("IsMultipleAnswer");
+                            IsMultipleAnswer=Boolean.parseBoolean(isMultiple);
+                            if(IsMultipleAnswer){
+                                createCheckBox(number, answerArray);
+                            }
+                            else{
+                                createRadioButton(number, answerArray);
+                            }
                         } catch (JSONException e) {
 
                             e.printStackTrace();
                         }
-
                     }
 
                 }, new Response.ErrorListener() {
@@ -129,37 +144,73 @@ public class QuestionActivity extends AppCompatActivity{
     public void send(View view) throws JSONException {
         String selection;
         try{
-            if(radioGroup.getCheckedRadioButtonId()!=-1){
-                int id= radioGroup.getCheckedRadioButtonId();
-                View radioButton = radioGroup.findViewById(id);
-                int radioId = radioGroup.indexOfChild(radioButton);
-                RadioButton btn = (RadioButton) radioGroup.getChildAt(radioId);
-                JSONObject answerobject= (JSONObject) btn.getTag();
-                final String questionId=answerobject.getString("QuestionId");
-                final String answerId=answerobject.getString("Id");
-                final JSONObject requestJsonObject = new JSONObject();
-                requestJsonObject.put("UserId","1");
-                requestJsonObject.put("QuestionId", questionId);
-                requestJsonObject.put("FirstAnswerId", answerId);
+            if(!IsMultipleAnswer){
+                if(radioGroup.getCheckedRadioButtonId()!=-1){
+                    int id= radioGroup.getCheckedRadioButtonId();
+                    View radioButton = radioGroup.findViewById(id);
+                    int radioId = radioGroup.indexOfChild(radioButton);
+                    RadioButton btn = (RadioButton) radioGroup.getChildAt(radioId);
+                    answerobject= (JSONObject) btn.getTag();
+                    final String questionId=answerobject.getString("QuestionId");
+                    final String answerId=answerobject.getString("Id");
+                    final JSONObject requestJsonObject = new JSONObject();
+                    requestJsonObject.put("UserId","1");
+                    requestJsonObject.put("QuestionId", questionId);
+                    requestJsonObject.put("FirstAnswerId", answerId);
 
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, "http://192.168.1.109/survey/api/answers",
-                        requestJsonObject, new Response.Listener<JSONObject>() {
+                    JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, "http://192.168.1.109/survey/api/answers",
+                            requestJsonObject, new Response.Listener<JSONObject>() {
 
-                    @Override
-                    public void onResponse(JSONObject jsonObject) {
-                        Toast.makeText(getApplicationContext(), jsonObject.toString(), Toast.LENGTH_LONG).show();
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            Toast.makeText(getApplicationContext(), jsonObject.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            Toast.makeText(getApplicationContext(), volleyError.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    AppController.getInstance().addToRequestQueue(request);
+                }
+                else{
+                    selection="select one pls!";
+                    Toast.makeText(getApplicationContext(),selection,Toast.LENGTH_LONG).show();
+                }
+            }
+            else if(IsMultipleAnswer){
+                for(int i=0; i<mLinearLayout.getChildCount(); i++) {
+                    View nextChild = mLinearLayout.getChildAt(i);
+                    if(nextChild instanceof CheckBox)
+                    {
+                        CheckBox check = (CheckBox) nextChild;
+                        if (check.isChecked()) {
+                            answerobject= (JSONObject) check.getTag();
+                            getAnswerArray.put(answerobject);
+                            Toast.makeText(getApplicationContext(),getAnswerArray.toString(),Toast.LENGTH_LONG).show();
+                            System.out.println(getAnswerArray);
+                        }
                     }
+
+                }
+                JSONObject answer=new JSONObject();
+                answer.put("answers",getAnswerArray);
+                System.out.println(answer);
+                JsonArrayRequest request=new JsonArrayRequest(Request.Method.POST, "http://192.168.1.109/survey/api/answers", getAnswerArray, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+                    }
+
                 }, new Response.ErrorListener() {
                     @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        Toast.makeText(getApplicationContext(), volleyError.toString(), Toast.LENGTH_LONG).show();
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
                     }
                 });
                 AppController.getInstance().addToRequestQueue(request);
-            }
-            else{
-                selection="select one pls!";
-                Toast.makeText(getApplicationContext(),selection,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),answerCollection.toString(),Toast.LENGTH_LONG).show();
+
             }
             if(index<sizeOfQuestionBank-1){
                 index++;
